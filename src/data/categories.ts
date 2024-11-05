a59e67cf51def046de31f9abb172da6c222e1b92
anclaamex <anclaamex@gmail.com>
2024-11-05 16:20:39 -0600

    nevos posts

--- src/data/categories.ts
+++ src/data/categories.ts
@@ -1,8 +1,8 @@
 // List of categories for blog posts
 export const CATEGORIES = [
-	'Category 1',
-	'Category 2',
-	'Category 3',
-	'Category 4',
-	'Category 5'
+	'Liga MX',
+	'La Liga',
+	'Premier League',
+	'Liga Italeana',
+	'Liga alemana'
 ] as const
